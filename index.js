1186a0613642674bf06d3c78f3e4d30fbe77fbba
Mércia de Souza Maguerroski <31809741+merciasm@users.noreply.github.com>
2020-09-10 23:06:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+var items = [
+  {
+    title: "",
+    image: "",
+    content: ""
+  },
+  {
+    title: "",
+    image: "",
+    content: ""
+  },
+  {
+    title: "",
+    image: "",
+    content: ""
+  }];
 
+items.forEach(createNewItem);
+
+function createNewItem(item, index) {
+  let div = document.createElement("div");
+  let h2 = document.createElement("h2");
+  let p = document.createElement("p");
+  let img = document.createElement("img");
+  
+}
